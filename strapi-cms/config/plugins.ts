ab5894ad6212702c5eae19b87487d6240f699721
lux-02 <oys128950@gmail.com>
2025-11-05 11:22:12 +0000

    fix to strapi err

--- strapi-cms/config/plugins.ts
+++ strapi-cms/config/plugins.ts
@@ -1,12 +1,5 @@
-export default ({ env }: { env: any }) => ({
-  upload: {
-    config: {
-      provider: 'strapi-provider-upload-cloudflare',
-      providerOptions: {
-        account_id: env('CLOUDFLARE_ACCOUNT_ID'),
-        api_token: env('CLOUDFLARE_API_TOKEN'),
-        variant: 'public', // Cloudflare Images variant for optimization
-      },
-    },
-  },
+export default () => ({
+  // Upload configuration
+  // Using default local upload for now
+  // Cloudflare Images integration can be added later with compatible plugin
 });
